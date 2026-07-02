6a0ced59986c385a97a15290921b46a086bf556c
syu950331@gmail.com <syu950331@gmail.com>
2026-07-02 14:21:01 +0900

    battle 코드 추가

--- battle.py
+++ battle.py
@@ -1,1 +1,13 @@
 "hellogit"
+class BattleManager:
+    def is_monster_dead(self, monster):
+        return monster.hp <= 0
+
+    def player_attack(self, player, monster):
+        player.attack(monster)
+
+        if self.is_monster_dead(monster):
+            print(f"{monster.name}을(를) 처치했습니다!")
+        else:
+            print(f"{monster.name}의 남은 HP: {monster.hp}")
+            
