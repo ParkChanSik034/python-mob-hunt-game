a06a83b09f1a0e19d66d695530854e7694f648f3
wldbs <yunns7981@gmail.com>
2026-07-02 15:30:32 +0900

    player issue 수정

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
-# player
+# player.py
+
 class Player:
-    def __init__(self, name, hp, attack_power):
+    def __init__(self, name, hp=100, attack_power=10):
         """
         플레이어 생성자
         :param name: 플레이어 이름 (str)
-        :param hp: 플레이어 체력 (int)
-        :param attack_power: 플레이어 공격력 (int)
+        :param hp: 플레이어 체력 (int), 기본값 100
+        :param attack_power: 플레이어 공격력 (int), 기본값 10
         """
         self.name = name
         self.hp = hp
@@ -15,18 +16,22 @@ class Player:
         """
         몬스터를 공격하여 몬스터의 HP를 감소시키는 메서드
         데미지 공식 = 플레이어 공격력 - 몬스터 방어력 (최소 1 보장)
+        몬스터의 HP는 0보다 작아지지 않음.
         """
         # 데미지 계산: 플레이어 공격력 - 몬스터 방어력
-        # monster 객체에 defense 속성이 있다고 가정합니다.
         damage = self.attack_power - monster.defense
         
         # 데미지가 1보다 작으면 최소 1로 처리
         if damage < 1:
             damage = 1
             
-        # 몬스터의 HP 감소 (monster 객체에 hp 속성이 있다고 가정합니다.)
+        # 몬스터의 HP 감소
         monster.hp -= damage
         
+        # [완료 조건 반영] 몬스터 HP가 0보다 작아지지 않도록 처리
+        if monster.hp < 0:
+            monster.hp = 0
+            
         print(f"⚔️ {self.name}(이)가 몬스터를 공격하여 {damage}의 데미지를 입혔습니다!")
 
     def info(self):
